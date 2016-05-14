9a0b1e41900f6b3bfa092289388ce4e21e004dab
alivelee <a871460434@gmail.com>
2016-05-14 15:57:04 +0800

    add multiline text input

--- Page/SplitPage.js
+++ Page/SplitPage.js
@@ -5,6 +5,7 @@ import {
   StyleSheet,
   View,
   Text,
+  TextInput,
   TouchableOpacity,
 } from 'react-native';
 
@@ -12,14 +13,39 @@ class SplitPage extends Component {
   render() {
     var navigator = this.props.navigator;
     return (
-      <View style={{backgroundColor: 'rgba(0,0,0,0.5)', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <View style={{backgroundColor: '#C8C8C4', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <TextInput
+            placeholder="multiline text input with font styles and placeholder"
+            multiline={true}
+            clearTextOnFocus={true}
+            autoCorrect={true}
+            autoCapitalize="words"
+            placeholderTextColor="red"
+            keyboardType="url"
+            style={[styles.multiline, styles.multilineWithFontStyles]}
+          />
         <TouchableOpacity
           onPress={() => navigator.pop()}>
-          <Text style={{color: 'yellow'}}>我真的没有导航栏，点我试试，非打死你不可！！</Text>
+          <Text style={{color: 'yellow'}}>TextInput</Text>
         </TouchableOpacity>
       </View>
     );
   }
 }
-
+const styles = StyleSheet.create({
+  multiline: {
+    borderWidth: 0.5,
+    borderColor: '#0f0f0f',
+    flex: 1,
+    fontSize: 13,
+    height: 50,
+    padding: 4,
+    marginBottom: 4,
+  },
+  multilineStyle: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 18,
+  }
+});
 module.exports = SplitPage;
